d9410fce111292f14af51504385835877362593f
Tim Smith <tsmith@chef.io>
2017-09-22 13:45:06 -0700

    Combine service and installation tests into one
    Shaves a chunk of time off running the local tests
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/service/serverspec/run_spec.rb
+++ test/integration/service/serverspec/run_spec.rb
@@ -2,6 +2,16 @@ require 'serverspec'
 
 set :backend, :exec
 
+describe command '/usr/sbin/flanneld -version' do
+  its :exit_status do
+    should eq 0
+  end
+
+  its :stderr do
+    should match(/0.5.5/)
+  end
+end
+
 describe file '/run/flannel/subnet.env' do
   it 'should propagate the network' do
     expect(subject.content).to match %r{FLANNEL_NETWORK=10.0.0.0/8}
